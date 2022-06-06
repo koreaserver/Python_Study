1a4f6c836d709fe3412cb3e24ac2eb1db5f580a7
KS <102852097+koreaserver@users.noreply.github.com>
2022-06-06 20:29:48 +0900

    Add files via upload

---
+++ ScoreSort.py
@@ -0,0 +1,20 @@
+
+def scoreSort(ary):
+	n = len(ary)
+	for end in range(1, n):
+		for cur in range(end, 0, -1):
+			if (ary[cur-1][1] > ary[cur][1]):
+				ary[cur-1], ary[cur] = ary[cur], ary[cur-1]
+	return ary
+
+
+scoreAry = [['선미', 88], ['초아', 99], ['화사', 71], ['영탁', 78], ['영웅', 67], ['민호', 92]]
+
+
+print('정렬 전 -->', scoreAry)
+scoreAry = scoreSort(scoreAry)
+print('정렬 후 -->', scoreAry)
+
+print('## 성적별 조 편성표 ##')
+for i in range(len(scoreAry)//2):
+	print(scoreAry[i][0], ':', scoreAry[len(scoreAry)-1-i][0])
